db6f6df9a60a828da408eb771852a4334c2c8136
andreskwan <andreskwan@gmail.com>
2016-05-07 21:53:41 -0700

    cs106a [as1 - CollectNewspaperKarel - extending from SuperKarol] - includes turnRight

--- CollectNewspaperKarel.java
+++ CollectNewspaperKarel.java
@@ -1,8 +1,8 @@
 /*
- * File: CollectNewspaperKarel.java
  * --------------------------------
  * At present, the CollectNewspaperKarel subclass does nothing.
  * Your job in the assignment is to add the necessary code to
+ * File: CollectNewspaperKarel.java
  * instruct Karel to walk to the door of its house, pick up the
  * newspaper (represented by a beeper, of course), and then return
  * to its initial position in the upper left corner of the house.
@@ -12,7 +12,7 @@ import stanford.karel.*;
 
 // Karol is defined as a class
 // here we are creating a custom Karol robot
-public class CollectNewspaperKarel extends Karel {
+public class CollectNewspaperKarel extends SuperKarel {
 	
 	// main method, entry point 
 	// where program starts running 
@@ -24,12 +24,6 @@ public class CollectNewspaperKarel extends Karel {
 		turnLeft();
 		move();
 		pickBeeper();
-	}
-	
-	private void turnRight(){
-		turnLeft();
-		turnLeft();
-		turnLeft();
 	}
 	
 }
