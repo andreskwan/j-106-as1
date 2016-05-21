b78c87088ad83d7f3b7d170c2e262cb43075e561
andreskwan <andreskwan@gmail.com>
2016-05-21 08:47:54 -0500

    cs106a [as1 - question2] solved - ToDo - side effect of method return and perform an action

--- StoneMasonKarel.java
+++ StoneMasonKarel.java
@@ -13,5 +13,56 @@ import stanford.karel.*;
 public class StoneMasonKarel extends SuperKarel {
 
 	// You fill in this part
-
+	public void run() {
+		/**
+		 * 1) completar columna
+		 * 2) volver a la base de la columna
+		 * 3) verificar si ultima columna //si ultima terminar 
+		 * 4) avanzar siguiente columna
+		 * 5) repetir
+		 */		
+		completeColum();
+		goToTheBase();
+		while (frontIsClear()) {
+			goToNextColumn();
+			completeColum();
+			goToTheBase();			
+		}
+	}
+	/**
+	 * 1) complete colum 
+	 * 1.1 
+	 */
+	private void completeColum() {
+		turnLeft();
+		while (frontIsClear()) {
+			checkAndPlaceBeeper();
+			move();			
+		}
+		checkAndPlaceBeeper();
+	}
+	private void checkAndPlaceBeeper() {
+		if (noBeepersPresent()) {
+			putBeeper();
+		}
+	}
+	private void goToTheBase() {
+		//guarantee to face south
+		if (facingNorth()) {
+			turnAround();		
+		}
+		while (frontIsClear()) {
+			move();
+		}
+		turnLeft();
+	}
+	private void goToNextColumn() {
+		for(int i = 0; i<4; i++){
+			if (frontIsClear()) {
+				move();
+			} else {
+				break;
+			}
+		}
+	}
 }
